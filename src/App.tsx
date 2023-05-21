e7bb96b929dedb05b2a07653b73c78b990a9174d
Gregory Monroe II <gregthemusician@gmail.com>
2023-05-20 22:25:29 -0400

    add code to detect MetaMask. Does exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+let injectedProvider = false
+
+if (typeof window.ethereum !== 'undefined') {
+  injectedProvider = true
+  console.log(window.ethereum)
+}
+
+const isMetaMask = injectedProvider ? window.ethereum.isMetaMask : false
 
 const App = () => {
 
   return (
     <div className="App">
-      <button>Connect MetaMask</button>
+      <h2>Injected Provider { injectedProvider ? 'DOES' : 'DOES NOT'} Exist</h2>
+      { isMetaMask && 
+        <button>Connect MetaMask</button>
+      }
     </div>
   )
 }
