1961d4269a6dc51d900047051389b8915eb8960b
Gregory Monroe II <gregthemusician@gmail.com>
2023-05-20 23:53:40 -0400

    add updateWallet

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import detectEthereumProvider from '@metamask/detect-provider'
 
 const App = () => {
   const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+  const initialState = { accounts:[] }
+  const [wallet, setWallet] = useState(initialState)
 
   useEffect(() => {
     const getProvider = async () => {
@@ -15,11 +17,27 @@ const App = () => {
     getProvider()
   }, [])
 
+  const updateWallet = async (account:any) => {
+    setWallet({ accounts })
+  }
+
+  const handleConnect = async () => {
+    let accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    })
+    updateWallet(accounts)
+  }
+
   return (
     <div className="App">
       <div>Injected Provider {hasProvider ? 'DOES' : 'DOES NOT'} Exist</div>
+      
       { hasProvider &&
         <button>Connect MetaMask</button>
+      }
+      
+      { wallet.accounts.length > 0 && 
+        <div>Wallet Accounts: { wallet.accounts[0] }</div>      
       }
     </div>
   )
